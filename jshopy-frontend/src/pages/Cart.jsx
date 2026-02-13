37b62ee4e350495a38a05bac722cf9ab097b3890
Jagatheshwaran N <jagatheshwaran.n29@gmail.com>
2026-02-12 20:43:55 -0600

    Cart Page Issue Fixed. Checkout Page Design Inprogress.

--- jshopy-frontend/src/pages/Cart.jsx
+++ jshopy-frontend/src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import { assets } from '../assets/assets';
 import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
-  const { products, currency, cartItems, updateCartQuantity } = useContext(ShopContext);
+  const { products, currency, cartItems, updateCartQuantity, navigate } = useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
 
   useEffect(() => {
@@ -34,7 +34,7 @@ const Cart = () => {
           cartData.map((item, index) => {
             const productData = products.find((product) => product._id === item._id);
             return (
-              <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+              <div div key={`${item._id}-${item.size}`} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
                 <div className='flex items-start gap-6'>
                   <img className='w-16 sm:w-20' src={productData.image[0]} alt='cartImage' />
                   <div>
@@ -45,7 +45,7 @@ const Cart = () => {
                     </div>
                   </div>
                 </div>
-                <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateCartQuantity(item._id, item.size, Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type='number' min={1} defaultValue={item.quantity} />
+                <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateCartQuantity(item._id, item.size, Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type='number' min={1} value={item.quantity} />
                 <img onClick={() => updateCartQuantity(item._id, item.size, 0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt='cartDelete' />
               </div>
             )
@@ -55,6 +55,9 @@ const Cart = () => {
       <div className='flex justify-end my-20'>
         <div className='w-full sm:w-[450px]'>
           <CartTotal />
+          <div className='w-full text-end'>
+            <button onClick={() => navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>Proceed To Checkout</button>
+          </div>
         </div>
       </div>
     </div>
