ed1e5dc4487093d15c0893423b25f6c482608f13
Jagatheshwaran N <jagatheshwaran.n29@gmail.com>
2026-02-18 21:10:23 -0600

    Jshoppy - Admin List Page Design Inprogress

--- jshopy-admin/src/pages/Add.jsx
+++ jshopy-admin/src/pages/Add.jsx
@@ -1,9 +1,10 @@
-import React, { use, useState } from 'react'
+import { useState } from 'react'
 import { assets } from '../assets/assets'
 import axios from 'axios'
 import { backendUrl } from '../App';
+import { toast } from 'react-toastify';
 
-const Add = ({token}) => {
+const Add = ({ token }) => {
   const [image1, setImage1] = useState(false);
   const [image2, setImage2] = useState(false);
   const [image3, setImage3] = useState(false);
@@ -34,15 +35,29 @@ const Add = ({token}) => {
       image3 && formData.append('image3', image3);
       image4 && formData.append('image4', image4);
 
-      const response = await axios.post(backendUrl+'/api/product/add', formData, {headers:{token}});
-      console.log(response.data);
+      const response = await axios.post(backendUrl + '/api/product/add', formData, { headers: { token } });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        setImage1(false);
+        setImage2(false);
+        setImage3(false);
+        setImage4(false);
+        setName('');
+        setDescription('');
+        setPrice('');
+        setSizes([]);
+        setBestSeller(false);
+      } else {
+        toast.error(response.data.message);
+      }
     } catch (error) {
-
+      console.log(error);
+      toast.error(error.message);
     }
   }
 
   return (
-    <form className='flex flex-col w-full items-start gap-3'>
+    <form onSubmit={onSubmitHandler} className='flex flex-col w-full items-start gap-3'>
       <div>
         <p className='mb-2'>Upload Image</p>
         <div className='flex gap-2'>
@@ -90,8 +105,8 @@ const Add = ({token}) => {
           </select>
         </div>
         <div>
-          <p onChange={(e) => setPrice(e.target.value)} value={price} className='mb-2'>Product Price</p>
-          <input className='w-full px-3 py-2 sm:w-[120px]' type='Number' placeholder='Price' />
+          <p className='mb-2'>Product Price</p>
+          <input onChange={(e) => setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' type='Number' placeholder='Price' />
         </div>
       </div>
       <div>
